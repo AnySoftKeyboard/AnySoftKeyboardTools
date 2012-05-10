b6cf3f8c31449fb8429ab8280ffd11df9713d124
Menny Even Danan <menny@evendanan.net>
2012-05-10 10:53:54 +0300

    fetch script fix

--- GitStuff/fetch-all.sh
+++ GitStuff/fetch-all.sh
@@ -5,5 +5,8 @@ f=`dirname "$0"`
 echo "Fetching from upstream to local respositories..."
 
 exec_at_folder "AnySoftKeyboard" "git fetch upstream"
+exec_at_folder "AnySoftKeyboard" "git merge upstream/master"
 exec_at_folder "AnySoftKeyboard-API" "git fetch upstream"
+exec_at_folder "AnySoftKeyboard-API" "git merge upstream/master"
 exec_at_folder "AnySoftKeyboardTools" "git fetch upstream"
+exec_at_folder "AnySoftKeyboardTools" "git merge upstream/master"
