42831322fb4114bb565c544937756c466a36a257
Menny Even Danan <menny@evendanan.net>
2016-11-03 16:57:13 -0400

    better progress output

--- generatewordslist/src/main/groovy/com/anysoftkeyboard/tools/generatewordslist/Parser.java
+++ generatewordslist/src/main/groovy/com/anysoftkeyboard/tools/generatewordslist/Parser.java
@@ -1,7 +1,18 @@
 package com.anysoftkeyboard.tools.generatewordslist;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
 
 class Parser {
 
@@ -12,21 +23,20 @@ class Parser {
     private final HashSet<Character> mLangChars;
     private final HashSet<Character> mLangInnerChars;
     private final HashMap<String, WordWithCount> mWords;
-    private final long mInputSize;
     private final int mMaxListSize;
     private final Locale mLocale;
+
     public Parser(List<File> inputFiles, File outputFile, char[] wordCharacters, Locale locale, char[] additionalInnerWordCharacters, int maxListSize) throws IOException {
-        if (inputFiles.size() == 0) throw new IllegalArgumentException("Files list should be at least 1 size.");
-        long totalFilesSize = 0;
+        if (inputFiles.size() == 0)
+            throw new IllegalArgumentException("Files list should be at least 1 size.");
         for (File inputFile : inputFiles) {
-            if (!inputFile.exists()) throw new IOException("Could not file input file " + inputFile);
+            if (!inputFile.exists())
+                throw new IOException("Could not file input file " + inputFile);
             if (!inputFile.isFile()) throw new IOException("Input must be a file.");
-            totalFilesSize += inputFile.length();
         }
         mInputFiles = Collections.unmodifiableList(inputFiles);
         mLocale = locale;
         mMaxListSize = maxListSize;
-        mInputSize = totalFilesSize;
         mOutput = new OutputStreamWriter(new FileOutputStream(outputFile));
 
         mLangInnerChars = new HashSet<>(additionalInnerWordCharacters.length + wordCharacters.length);
@@ -49,13 +59,13 @@ class Parser {
         for (File inputFile : mInputFiles) {
             System.out.println(String.format(Locale.US, "Reading input file %s...", inputFile));
             InputStreamReader inputStream = new FileReader(inputFile);
-            addWordsFromInputStream(inputStream);
+            addWordsFromInputStream(inputFile.length(), inputStream);
             inputStream.close();
+            System.out.println(String.format(Locale.US, "Have %d words so far.", mWords.size()));
         }
 
         System.out.println("Sorting list...");
-        List<WordWithCount> sortedList = new ArrayList<>(Math.min(mWords.size(), mMaxListSize));
-        sortedList.addAll(mWords.values());
+        List<WordWithCount> sortedList = new ArrayList<>(mWords.values());
         Collections.sort(sortedList);
 
         System.out.println("Creating output XML file...");
@@ -76,7 +86,7 @@ class Parser {
             WordWithCount word = sortedList.get(wordIndex);
             writer.writeEntity("w").writeAttribute("f", Integer.toString(calcActualFreq(wordIndex, wordsCount))).writeText(word.getWord()).endEntity();
         }
-        System.out.println("Wrote "+wordsCount+" words.");
+        System.out.println("Wrote " + wordsCount + " words.");
         writer.endEntity();
     }
 
@@ -84,15 +94,15 @@ class Parser {
         return Math.min(255, 1 + (int) (255 * (wordsCount - wordIndex) / wordsCount));
     }
 
-    private void addWordsFromInputStream(InputStreamReader input) throws IOException {
+    private void addWordsFromInputStream(final long inputSize, InputStreamReader input) throws IOException {
         StringBuilder word = new StringBuilder();
         int intChar;
 
         int state = LOOKING_FOR_WORD_START;
-        int read = 0;
+        long read = 0;
         while ((intChar = input.read()) > 0) {
-            if ((read % 50000) == 0) {
-                System.out.println("Read " + read + " out of " + mInputSize + " (" + ((100 * read) / mInputSize) + "%)...");
+            if ((read % 50000) == 0 || read == inputSize) {
+                System.out.println("Read " + read + " out of " + inputSize + " (" + ((100 * read) / inputSize) + "%)...");
             }
             char currentChar = (char) intChar;
             read++;
